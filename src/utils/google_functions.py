126595299903668f12dcc727204b0f717dce4cb8
Brian Murray <murraycoding@gmail.com>
2025-01-30 12:48:45 -0500

    added google_functions.py to utils

---
+++ src/utils/google_functions.py
@@ -0,0 +1,115 @@
+""" Description: This file contains the code to update a google sheet """
+### IMPORTS ###
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def get_google_creds():
+    """
+    Function to get the google credentials
+    """
+    creds = None
+    SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+    token_path = "src/trail_life/token.json"
+    credentials_path = "src/trail_life/credentials.json"
+        # 
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(token_path):
+        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                credentials_path, SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(token_path, "w") as token:
+            token.write(creds.to_json())
+
+    return creds
+
+def update_google_sheet(sheet_id, range_name, data):
+    """
+    Function to update a google sheet
+    """
+    creds = get_google_creds()
+
+    print(" - - - - - - UPDATING GOOGLE SHEET - - - - - - ")
+    print()
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .update(spreadsheetId=sheet_id,
+                    range=range_name,
+                    valueInputOption="USER_ENTERED",
+                    body=dict(
+                    majorDimension='ROWS',  # The first dimension of the values array corresponds to rows
+                    values=data  # The data to input
+            ))
+            .execute()
+        )
+        values = result.get("values",[])
+
+        if not values:
+            print(" - - - - - - UPDATED COMPLETE (No data returned)- - - - - -")
+            print()
+            return
+
+        print(" - - - - - - UPDATED COMPLETE - - - - - -")
+        print()
+
+        return values
+
+    except HttpError as http_error:
+        print(f"An http error occurred: {http_error}")
+
+def get_google_sheets_data(sheet_id, range_name):
+    """
+    Function to get Google Sheets data
+
+    :param sheet_id: str: The sheet ID
+    :param range_name: str: The range of the sheet
+
+    :return: list: The data from the sheet
+    """
+    print(" - - - - - GETTING GOOGLE SHEETS DATA - - - - - ")
+    print()
+    creds = get_google_creds()
+
+    try:
+        service = build("sheets", "v4", credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = (
+            sheet.values()
+            .get(
+                spreadsheetId=sheet_id,
+                range=range_name,
+                dateTimeRenderOption="FORMATTED_STRING",
+                majorDimension="ROWS",
+                valueRenderOption="FORMATTED_VALUE"
+            )
+            .execute()
+        )
+        values = result.get("values", [])
+        
+        print(" - - - - - - DATA RETRIEVED - - - - - - ")
+        print()
+        return values
+
+    except HttpError as http_error:
+        print(f"An http error occurred: {http_error}")
