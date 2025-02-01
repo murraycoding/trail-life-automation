9479b34b49e4ffd761e9c9ff4942d76103cdd4bc
Brian Murray <murraycoding@gmail.com>
2025-01-31 22:53:36 -0500

    added some template code

---
+++ attendance_report.py
@@ -0,0 +1,16 @@
+"""
+File to be run to generate the attendance report
+"""
+
+def attendance_report(env):
+    # open the latest attendance report
+
+    # covert data in the spreadsheet into a dataframe
+
+    # complete the following tasks as a part of the report
+    # 1. Get a list of the last date each trailman attended a meeting
+    # 2. Get a list of the number of meetings each trailman has attended separated by the current year and last
+    pass
+
+if __name__ == "__main__":
+    attendance_report("DEV")
