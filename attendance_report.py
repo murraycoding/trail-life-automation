71b9695341edd8864e54ceaaf8bf4148c1c4d723
Brian Murray <murraycoding@gmail.com>
2025-02-01 16:18:57 -0500

    added Google Sheet information

--- attendance_report.py
+++ attendance_report.py
@@ -1,9 +1,17 @@
 """
 File to be run to generate the attendance report
 """
+import json
 
 def attendance_report(env):
+    # data setup
+    with open("google_sheets_id_data.json") as file:
+        google_id_data = json.load(file)
+    
+    attendance_sheets_id = google_id_data[env]["attendance_report_sheets_id"]
+
     # open the latest attendance report
+    
 
     # covert data in the spreadsheet into a dataframe
 
